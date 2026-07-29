0be7b22a72863384930d9edf3fc601fc09413959
fallinblossoms777 <166082974+fallinblossoms777@users.noreply.github.com>
2026-07-28 23:35:58 -0300

    Complete sumAll exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let array = [num1, num2].sort((a,b) => a - b);
+    let result = 0;
+    for (let i = 1; i <= array[1]; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
