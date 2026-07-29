6ca01ba7c1b8b406278ce67b3332c93a7118a640
fallinblossoms777 <166082974+fallinblossoms777@users.noreply.github.com>
2026-07-29 17:10:21 -0300

    Complete leapYears excercise

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if ( (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0) )
+        return true;
+    else
+        return false;
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+/*
+Determinar si un año es bisiesto.
+Una año es bisiesto cuando:
+.Es divisible por 4 y no es divisible por 100.
+o
+.Es divisible por 400.
+if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+*/
